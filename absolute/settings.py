384b11eb3ac059d3466e429e81e2f5297f513531
Andrey Blinov <blinov@itsl.ru>
2017-11-24 17:02:58 +0300

    try deploy

--- absolute/settings.py
+++ absolute/settings.py
@@ -177,12 +177,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_ROOT = '/static_root/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = ( os.path.join('static'), )
-
 
 LOCALE_PATHS = ( os.path.join(BASE_DIR, 'locale'), )
 
