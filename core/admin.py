5a4dc453b74da128d817c7bcf4951a43355ceb6a
Andrey Blinov <blinov@itsl.ru>
2017-11-25 07:51:17 +0300

    fix admin

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from nested_admin import NestedModelAdmin, NestedStackedInline
 from core.models import *
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import Group
+from push_notifications.models import APNSDevice, GCMDevice, WNSDevice
 
 class SheduleInline(NestedStackedInline):
     model = Shedule
@@ -8,3 +11,9 @@ class SheduleInline(NestedStackedInline):
 @admin.register(User)
 class UserAdmin(NestedModelAdmin):
 	inlines = [SheduleInline]
+
+admin.site.unregister(Token)
+admin.site.unregister(Group)
+admin.site.unregister(APNSDevice)
+admin.site.unregister(GCMDevice)
+admin.site.unregister(WNSDevice)
