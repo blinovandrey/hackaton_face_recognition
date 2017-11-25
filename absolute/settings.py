88c1ee29d925ae708561110b732ff93b29a9d1b3
Andrey Blinov <blinov@itsl.ru>
2017-11-25 03:23:22 +0300

    add parsers

--- absolute/settings.py
+++ absolute/settings.py
@@ -109,7 +109,12 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
-    )
+    ),
+    'DEFAULT_PARSER_CLASSES': (
+        'rest_framework.parsers.JSONParser',
+        'rest_framework.parsers.MultiPartParser',
+        'rest_framework.parsers.FileUploadParser',
+    ),
 }
 
 
